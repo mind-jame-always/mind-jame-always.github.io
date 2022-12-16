3e03c0ac427a175dfc66630307df56172f26e0d5
aefnattanon <aef.nattanon@gmail.com>
2022-12-16 21:41:43 +0700

    Init template

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 04, 13)}); // year, month, date, hour
+        });
+});		
+
