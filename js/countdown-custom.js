4507b826bb5dedf7e8ea14b115b4a429d5e08136
aefnattanon <aef.nattanon@gmail.com>
2022-12-16 23:34:54 +0700

    puth

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
   jQuery(function () {
-    jQuery("#defaultCountdown").countdown({ until: new Date(2022, 12, 23) }); // year, month, date, hour
+    jQuery("#defaultCountdown").countdown({ until: new Date(2022, 11, 23) }); // year, month, date, hour
   });
 });
